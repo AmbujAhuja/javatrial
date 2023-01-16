aecbbd7fb34f99ef25a2c65bf90a23c77514c084
AmbujAhuja <ambujahuja731@gmail.com>
2023-01-16 13:20:45 +0530

    pw.java modified

--- add.java
+++ add.java
@@ -8,5 +8,12 @@ public class add {
         
         System.out.println(result);
     }
-    
+    public static void main(String args[])
+    {
+        int num1 = 7;
+        int num2 = 5;
+        int result = num1 + num2;
+        
+        System.out.println(result);
+    }
 }
